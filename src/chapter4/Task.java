f68c5128efff687a133f48a34241656692af188e
yanbo0039 <yanbo0039@yeah.net>
2019-07-03 18:38:47 +0800

    chapter4

--- src/chapter4/Task.java
+++ src/chapter4/Task.java
@@ -1,5 +1,7 @@
 package chapter4;
 
+import java.util.Arrays;
+
 public class Task {
 	/*
 	 * 1. 编写一个简单程序，要求数组长度为5
@@ -54,14 +56,40 @@ public class Task {
 	}
 	
 	/*
-	 * 4. 有2个多维数组分别是 2 3 4 和 
-	 * 1 5 2 8 4 6 8 5 9 10 -3 2 7 -5 -18
+	 * 4. 有2个多维数组分别是
+		|  arr1  |   arr2  |
+		|----|------|
+		|2 3 4 | 1 5 2 8 |
+		|4 6 8 | 5 9 10 3 |
+		|       | 2 7 -5 -18|
+	 * 
 	 * 按照如下方式进行运算。生成一个2行4列的数组。
 	 * 此数组的第1行1列是2*1+3*5+4*2第1行2列是2*5+3*9+4*7
 	 * 第2行1列是4*1+6*5+8*2 依次类推。
 	 * （知识点：多维数组定义和创建、数组遍历、数组元素访问）
 	 */
-	
+	public static void arraySum() {
+		int[][] arr1 = {{2,3,4},{4,6,8}};
+		int[][] arr2 = {{1,5,2,8},{5,9,10,3},{2,7,-5,-18}};
+		
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr2[0].length; j++) {
+				int sum = 0;
+				String tempStr = "";
+				for (int j2 = 0; j2 < arr2.length; j2++) {
+					sum += arr1[i][j2] * arr2[j2][j];
+					if(j2 == arr2.length-1) {
+						tempStr += arr1[i][j2] + "*" + arr2[j2][j];
+					}else {
+						tempStr += arr1[i][j2] + "*" + arr2[j2][j] + "+";
+					}
+				}
+				System.out.print(tempStr +"="+sum+" ");
+			
+			}
+			System.out.println();
+		}
+	}
 	/*
 	 * 5. 输出一个double型二维数组（长度分别为5、4，值自己设定）的值。
 	 * （知识点：数组定义和创建、多维数组初始化、数组遍历）
@@ -120,8 +148,6 @@ public class Task {
 		for(int i = index - 2;i >= 0;i--) {
 			System.out.print(myList[i] + " ");
 		}
-		
-		
 	}
 	/*
 	 * 8. 将一个数组中的重复元素保留一个其他的清零。
@@ -141,14 +167,52 @@ public class Task {
 		System.out.println(java.util.Arrays.toString(myList));
 	}
 	
+	/*
+	 * 9、给定一维数组{ -10，2，3，246，-100，0，5} ， 
+	 * 计算出数组中的平均值、最大值、最小值。 
+	 * （知识点：数组遍历、数组元素访问）
+	 */
+	public static void calcArray() {
+		int[] arr = {-10,2,3,246,-100,0,5};
+		Arrays.sort(arr);
+		System.out.println("数组arr中的最大值是： "+arr[arr.length-1]);
+		System.out.println("数组arr中的最小值是： "+arr[0]);
+		double sum = 0.0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		System.out.println(sum);
+		System.out.println("数组arr的平均值是： "+sum/arr.length);
+		
+//		 int[] arr ;
+//	        arr = new int[]{-10,2,3,246,-100,0,5};
+//
+//	        int sum = 0;
+//	        double avg;
+//	        int max = 0;
+//	        int min = 0;
+//
+//	        for(int i = 0 ;i<arr.length - 1;i++){
+//	            sum = sum +arr[i];
+//	            if(arr[i]>max){
+//	                max = arr[i];
+//	            }if(arr[i]<min){
+//	                min = arr[i];
+//	            }
+//	        }
+//	        avg = sum/arr.length;
+//	        System.out.println("数组中最大的数为："+max+"\n数组中最小的数为："+min+"\n数组中平均数为："+avg);
+	}
 	public static void main(String[] args) {
 //		crearArray();//1
 //		copyArray();//2
 //		sortArray();//3
 //		bubbleSortArray();//3
+//		arraySum();//4
 //		creatTwoDimensionalArray();//5
 //		maxNumAndIndex();//6
 //		outPutEleByDesc();//7
 //		toRpeatEle();//8
+//		calcArray();//9
 	}
 }
